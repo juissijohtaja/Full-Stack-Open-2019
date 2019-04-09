8114deebc3bb0062eacbd2a69602128faf01e85a
JuissiJohtaja <juha.koivu@outlook.com>
2019-04-09 14:53:22 +0300

    Done

--- Osa1/unicafe/src/index.js
+++ Osa1/unicafe/src/index.js
@@ -1,12 +1,94 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Button = (props) => (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Statistic = props => <tr><td>{props.text}</td><td>{props.value}</td></tr>
+
+const Statistics = (props) => {
+    const good = props.good
+    const neutral = props.neutral
+    const bad = props.bad
+
+    const total = good + neutral + bad
+    const countPositive = () => {
+        const pospercent = 100 * good / total
+        if (total > 0) {
+            return Math.round(pospercent * 100) / 100 + " %"
+        }
+        return 0;
+    }
+    const countMean = () => {
+        const mean = ((good * 1) - (bad * 1)) / total
+        if (total > 0) {
+            return Math.round(mean * 100) / 100
+        }
+        return 0
+    }
+    console.log(total)
+    if (total > 0) {
+        return (
+            <div>
+                <table>
+                    <tbody>
+                        <Statistic text="Hyvä" value={good} />
+                        <Statistic text="Neutraali" value={neutral} />
+                        <Statistic text="Huono" value={bad} />
+                        <Statistic text="Yhteensä" value={total} />
+                        <Statistic text="Keskiarvo" value={countMean()} />
+                        <Statistic text="Positiivisia" value={countPositive()} />
+                </tbody>
+                </table>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <p>Ei yhtään palautetta annettu</p>
+            </div>
+        )
+    }
+  }
+
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  const setToValueGood = newValue => {
+        setGood(newValue)    
+  }
+  const setToValueNeutral = newValue => {
+    setNeutral(newValue)    
+  }
+  const setToValueBad = newValue => {
+    setBad(newValue)    
+  }
+
+  return (
+    <div>
+      <h2>Anna palautetta</h2>
+      <Button handleClick={() => setToValueGood(good + 1)} text="Hyvä" />
+      <Button handleClick={() => setToValueNeutral(neutral + 1)} text="Neutraali" />     
+      <Button handleClick={() => setToValueBad(bad + 1)} text="Huono" />
+
+      <h2>Statistiikka</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
+      <Button handleClick={() => setToValueGood(0) & setToValueNeutral(0) & setToValueBad(0)} text="Nollaa kaikki" />
+      <Button handleClick={() => setToValueGood(good + 10) & setToValueNeutral(neutral + 10) & setToValueBad(bad + 10)} text="Lisää 10 kaikkiin" />
+
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
