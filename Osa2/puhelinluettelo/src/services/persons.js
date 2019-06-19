9bfa5786c1d6401148ff95685b333b53e51e7518
JuissiJohtaja <juha.koivu@outlook.com>
2019-06-19 17:21:30 +0300

    Done

--- Osa2/puhelinluettelo/src/services/persons.js
+++ Osa2/puhelinluettelo/src/services/persons.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+
+//const baseUrl = 'http://localhost:3001/api/persons'
+//const baseUrl = 'https://thawing-brook-56047.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
